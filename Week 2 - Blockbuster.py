d12436c3d7bc03dff45357ab6a9016631074ec91
Ryan J Morrow <ryan-morrow@uiowa.edu>
2024-09-05 17:09:05 -0500

    Update Week 2 - Blockbuster.py

--- Week 2 - Blockbuster.py
+++ Week 2 - Blockbuster.py
@@ -47,25 +47,22 @@ while attempted_password != correct_password:
         print("ERROR: This password is incorrect, please try again")
     else:
         print("Password is correct, welcome to your account")
-
-
-
-    
     
 # Problem: Check if someone is old enough to enter a restricted area (e.g., 18+)
 
-age = int(input("How old are you?")
-if age>=18:
+age = int(input("How old are you? "))
+          
+if age >= 18:
     print("You are old enough to enter this area, welcome.")
 else:
-    print("You are not old enough to enter this area, please try again in", 18-age,"years.") 
+    print("You are not old enough to enter this area, please try again in", 18 - age, "years.")
     
     
 # Input the name of the movie, its budget, and box office sales
 
 movie = input("What is the name of the movie you would like to analyze? ")
-budget = float(input("In millions of dollars, what was the budget of the movie? M$")
-sales = float(input("In millions of dollars, what was the total box office sales of the movie? M$")
+budget = float(input("In millions of dollars, what was the budget of the movie? M$"))
+sales = float(input("In millions of dollars, what was the total box office sales of the movie? M$"))
 
 # Calculate the sales multiplier 
 
@@ -76,11 +73,11 @@ sales_multiplier = sales / budget
 # Determine if the movie was a hit or bomb based on the sales multiplier
 
 if round(sales_multiplier,1) >= 3.0:
-    print("The total sales of this movie was", sales, "(M$), the budget was", budget, "(M$), this makes it a Blockbuster Hit."
+    print("The total sales of this movie was", sales, "(M$), the budget was", budget, "(M$), this makes it a Blockbuster Hit.")
 elif round(sales_multiplier,1) <= 1.5:
-    print("The total sales of this movie was", sales, "(M$), the budget was", budget, "(M$), this makes it a Blockbuster Bomb."
+    print("The total sales of this movie was", sales, "(M$), the budget was", budget, "(M$), this makes it a Blockbuster Bomb.")
 else:
-    print("The total sales of this movie was", sales, "(M$), the budget was", budget, "(M$), this makes it an average performing movie."
+    print("The total sales of this movie was", sales, "(M$), the budget was", budget, "(M$), this makes it an average performing movie.")
 
 # Print the return of investment for the movie
 
@@ -88,5 +85,3 @@ profit = sales - budget
 roi = (profit/budget)*100
 roi_str = str(round(roi,2))+"%"
 print("The ROI of", movie, "was", roi_str+".")
-
-
