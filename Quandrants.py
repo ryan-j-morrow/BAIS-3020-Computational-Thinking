a8f51b55908e5f7591bf6f179b3b3eb854a33070
Ryan J Morrow <ryan-morrow@uiowa.edu>
2024-09-11 17:17:49 +0000

    Update

--- Quandrants.py
+++ Quandrants.py
@@ -1,1 +1,12 @@
-
+x = float(input("Please enter an X value: "))
+y = float(input("Please enter an Y value: "))
+if x > 0 and y > 0
+    print("This cordinate is in Quadrant 1.")
+elif x < 0 and y > 0:
+    print("This cordinate is in Quadrant 2.")
+elif x < 0 and y < 0:
+    print("This cordinate is in Quadrant 3.")
+elif x > 0 and y < 0:
+    print("This cordinate is in Quadrant 4.")
+else:
+    print("This cordinate is in multiple quadrants.")
